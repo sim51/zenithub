a7db2879762f45937f4d981a13ff9202bd19a57f
Benoît Simard <bsimard@logisima.com>
2012-10-30 20:23:45 +0100

    adding cache for stats api

--- api/app/controllers/Application.java
+++ api/app/controllers/Application.java
@@ -42,80 +42,81 @@ public class Application extends Controller {
             // Do github async call for commits
             Promise<HttpResponse> futurResponse = WS.url(url).authenticate(ghUser, ghPwd).getAsync();
             res = await(futurResponse);
-        }
 
-        // List of all async response
-        List<Promise<HttpResponse>> futurCommits = new ArrayList<Promise<HttpResponse>>();
+            // List of all async response
+            List<Promise<HttpResponse>> futurCommits = new ArrayList<Promise<HttpResponse>>();
 
-        // parse the response
-        if (res.success()) {
-            JsonArray commits = res.getJson().getAsJsonArray();
-            for (int i = 0; i < commits.size(); i++) {
+            // parse the response
+            if (res.success()) {
+                JsonArray commits = res.getJson().getAsJsonArray();
+                for (int i = 0; i < commits.size(); i++) {
 
-                JsonObject commit = commits.get(i).getAsJsonObject();
+                    JsonObject commit = commits.get(i).getAsJsonObject();
 
-                // Do github async call for commit
-                String commitUrl = commit.get("url").getAsString();
-                futurCommits.add(WS.url(commitUrl).authenticate(ghUser, ghPwd).getAsync());
-            }
-            F.Promise<List<WS.HttpResponse>> promises = F.Promise.waitAll(futurCommits);
-            List<WS.HttpResponse> httpResponses = await(promises);
+                    // Do github async call for commit
+                    String commitUrl = commit.get("url").getAsString();
+                    futurCommits.add(WS.url(commitUrl).authenticate(ghUser, ghPwd).getAsync());
+                }
+                F.Promise<List<WS.HttpResponse>> promises = F.Promise.waitAll(futurCommits);
+                List<WS.HttpResponse> httpResponses = await(promises);
 
-            // key : login | value : its position into response arraylist
-            Map<String, Integer> authorStack = new HashMap<String, Integer>();
-            List<Impact> response = new ArrayList<Impact>();
+                // key : login | value : its position into response arraylist
+                Map<String, Integer> authorStack = new HashMap<String, Integer>();
+                List<Impact> response = new ArrayList<Impact>();
 
-            for (int j = 0; j < httpResponses.size(); j++) {
-                HttpResponse commitResponse = httpResponses.get(j);
-                if (commitResponse.success()) {
-                    JsonObject object = commitResponse.getJson().getAsJsonObject();
-                    if (!object.get("author").isJsonNull()) {
-                        JsonElement author = object.get("author");
-                        Logger.debug("Parsing commit " + object.get("url").getAsString() + " of "
-                                + author.getAsJsonObject().get("login").getAsString());
+                for (int j = 0; j < httpResponses.size(); j++) {
+                    HttpResponse commitResponse = httpResponses.get(j);
+                    if (commitResponse.success()) {
+                        JsonObject object = commitResponse.getJson().getAsJsonObject();
+                        if (!object.get("author").isJsonNull()) {
+                            JsonElement author = object.get("author");
+                            Logger.debug("Parsing commit " + object.get("url").getAsString() + " of "
+                                    + author.getAsJsonObject().get("login").getAsString());
 
-                        // is user already passed ?
-                        String login = author.getAsJsonObject().get("login").getAsString();
-                        if (!authorStack.containsKey(login)) {
-                            Impact impact = new Impact();
-                            impact.author = author;
-                            response.add(impact);
-                            authorStack.put(login, response.size() - 1);
-                        }
+                            // is user already passed ?
+                            String login = author.getAsJsonObject().get("login").getAsString();
+                            if (!authorStack.containsKey(login)) {
+                                Impact impact = new Impact();
+                                impact.author = author;
+                                response.add(impact);
+                                authorStack.put(login, response.size() - 1);
+                            }
 
-                        Impact impact = response.get(authorStack.get(login));
+                            Impact impact = response.get(authorStack.get(login));
 
-                        // retrive response commit data
-                        Integer deletions = object.get("stats").getAsJsonObject().get("deletions").getAsInt();
-                        Integer additions = object.get("stats").getAsJsonObject().get("additions").getAsInt();
-                        JsonObject commit = object.get("commit").getAsJsonObject();
+                            // retrive response commit data
+                            Integer deletions = object.get("stats").getAsJsonObject().get("deletions").getAsInt();
+                            Integer additions = object.get("stats").getAsJsonObject().get("additions").getAsInt();
+                            JsonObject commit = object.get("commit").getAsJsonObject();
 
-                        // adding commit
-                        impact.commits.add(commit);
+                            // adding commit
+                            impact.commits.add(commit);
 
-                        // Additions
-                        if (impact.impacts.containsKey(ADDITIONS)) {
-                            impact.impacts.put(ADDITIONS, impact.impacts.get(ADDITIONS) + additions);
-                        }
-                        else {
-                            impact.impacts.put(ADDITIONS, additions);
-                        }
+                            // Additions
+                            if (impact.impacts.containsKey(ADDITIONS)) {
+                                impact.impacts.put(ADDITIONS, impact.impacts.get(ADDITIONS) + additions);
+                            }
+                            else {
+                                impact.impacts.put(ADDITIONS, additions);
+                            }
 
-                        // Deletions
-                        if (impact.impacts.containsKey(DELETIONS)) {
-                            impact.impacts.put(DELETIONS, impact.impacts.get(DELETIONS) + deletions);
-                        }
-                        else {
-                            impact.impacts.put(DELETIONS, deletions);
+                            // Deletions
+                            if (impact.impacts.containsKey(DELETIONS)) {
+                                impact.impacts.put(DELETIONS, impact.impacts.get(DELETIONS) + deletions);
+                            }
+                            else {
+                                impact.impacts.put(DELETIONS, deletions);
+                            }
                         }
                     }
                 }
-            }
 
-            // last loop to do nb commit
-            for (int j = 0; j < response.size(); j++) {
-                Impact impact = response.get(j);
-                impact.impacts.put(NB_COMMITS, impact.commits.size());
+                // last loop to do nb commit
+                for (int j = 0; j < response.size(); j++) {
+                    Impact impact = response.get(j);
+                    impact.impacts.put(NB_COMMITS, impact.commits.size());
+                }
+                Cache.add(url, response, "1h");
             }
             renderJSON(response);
         }
