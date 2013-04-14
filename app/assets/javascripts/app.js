bf88128b97241600529d7ed03edac43ed6748648
Benoît Simard <bsimard@logisima.com>
2013-04-14 13:38:22 +0200

    'play2'ify application step4

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -5,13 +5,13 @@ var app = angular.module('zenithub', ['github', 'AwesomeChartJS']);
 
 app.config(function($routeProvider) {
 		$routeProvider
-			.when('/error', {templateUrl: 'partials/error.html', controller: ErrorCtrl})
-	      	.when('/repo/:owner/:repository', {templateUrl: 'partials/repository.html', controller: RepositoryHomeCtrl})
-	      	.when('/repo/:owner/:repository/members', {templateUrl: 'partials/members.html', controller: RepositoryMembersCtrl})
-	      	.when('/repo/:owner/:repository/commits', {templateUrl: 'partials/commits.html', controller: RepositoryCommitsCtrl})
-	      	.when('/repo/:owner/:repository/stats/geo', {templateUrl: 'partials/map.html', controller: RepositoryStatsGeoCtrl})
-	      	.when('/repo/:owner/:repository/stats/commit', {templateUrl: 'partials/stats_commit.html', controller: RepositoryStatsCommitCtrl})
-	      	.when('/search/:keyword', {templateUrl: 'partials/search.html',   controller: RepositoryListCtrl})
+			.when('/error', {templateUrl: '/assets/partials/error.html', controller: ErrorCtrl})
+	      	.when('/repo/:owner/:repository', {templateUrl: '/assets/partials/repository.html', controller: RepositoryHomeCtrl})
+	      	.when('/repo/:owner/:repository/members', {templateUrl: '/assets/partials/members.html', controller: RepositoryMembersCtrl})
+	      	.when('/repo/:owner/:repository/commits', {templateUrl: '/assets/partials/commits.html', controller: RepositoryCommitsCtrl})
+	      	.when('/repo/:owner/:repository/stats/geo', {templateUrl: '/assets/partials/map.html', controller: RepositoryStatsGeoCtrl})
+	      	.when('/repo/:owner/:repository/stats/commit', {templateUrl: '/assets/partials/stats_commit.html', controller: RepositoryStatsCommitCtrl})
+	      	.when('/search/:keyword', {templateUrl: '/assets/partials/search.html',   controller: RepositoryListCtrl})
 	      	.otherwise({redirectTo: '/search/'});
 	});
 
